dbd6fa94f1179b51956fec155df6f5230b3c02ae
Christopher Garcia <Christopherg1818@gmail.com>
2026-02-05 21:54:15 -0600

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;  // Fixed capitalization
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate is called after all Update functions
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + offset;  // Fixed missing dot
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpForce = 15f;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        // This connects the script to the Rigidbody component
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        // Makes the Player jump
+        if (Input.GetKeyDown(KeyCode.Space))
+        { 
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    // Detects when the Player overlaps with the coin
+    void OnTriggerEnter(Collider other) 
+    {
+        // IMPORTANT: Make sure your coin tag is exactly "coin" in Unity!
+        if (other.gameObject.CompareTag("coin"))
+        {
+            other.gameObject.SetActive(false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+}
+
